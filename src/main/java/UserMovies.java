be594afa4d6f92c87fdbd370d7e360596f622eba
kerwinzhou <zhouz88@uw.edu>
2017-12-24 20:20:01 -0800

    uygydfsaf

--- src/main/java/UserMovies.java
+++ src/main/java/UserMovies.java
@@ -1,6 +1,7 @@
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
@@ -15,10 +16,10 @@ import java.util.Iterator;
 
 public class UserMovies {
 
-    public static class UserMapper extends Mapper<Object, Text, IntWritable, Text> {
+    public static class UserMapper extends Mapper<LongWritable, Text, IntWritable, Text> {
 
         @Override
-        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
             String input = value.toString().trim();
             String[] line = input.split(":");
 
